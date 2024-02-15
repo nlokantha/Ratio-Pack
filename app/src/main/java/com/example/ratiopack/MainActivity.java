2b32e6f70b30a42bf4158873f1661ceb6ec8a55a
nlokantha <nlokantha@gmail.com>
2024-02-15 16:45:22 +0530

    Request Permission

--- app/src/main/java/com/example/ratiopack/MainActivity.java
+++ app/src/main/java/com/example/ratiopack/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.ratiopack;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -21,13 +29,34 @@ public class MainActivity extends AppCompatActivity {
     TextInputLayout textInputLayoutBuyer,textInputLayoutMethod;
     MaterialAutoCompleteTextView tv_buyer,tv_method;
     AppCompatButton btn_next;
+    public static final int REQUEST_CODE_WRITE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
-        requestStoragePermission();
+//        requestStoragePermission();
+//        2024-02-15.............
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE ) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,Manifest.permission.BLUETOOTH_CONNECT)){
+
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setMessage("We Need The Permission For.....")
+                            .setCancelable(true)
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_CODE_WRITE);
+                                }
+                            }).show();
+
+            }else {
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_CODE_WRITE);
+            }
+
+        }
 
         btn_next.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,5 +93,39 @@ public class MainActivity extends AppCompatActivity {
     }
     private void requestStoragePermission() {
         ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_WRITE){
+            if (grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+
+            }else {
+                if (!ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,Manifest.permission.BLUETOOTH_CONNECT)){
+//                DENIED PERMISSION....
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setMessage("You have Permanently denied this permission.,Go to Settings to Enable it.....")
+                            .setPositiveButton("Go to Settings", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    gotoApplicationSettings();
+                                }
+                            })
+                            .setNegativeButton("Cancle",null)
+                            .setCancelable(false)
+                            .show();
+                }else {
+                }
+            }
+        }
+    }
+
+    private void gotoApplicationSettings(){
+        Intent intent=new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri=Uri.fromParts("package",this.getPackageName(),null);
+        intent.setData(uri);
+        startActivity(intent);
     }
 }
